8839835ec02304ae2140f8101203cc9b7b7a95b1
jahnavi <jahnavi.kallichanda@gmail.com>
2020-05-25 12:13:21 -0600

    Second version

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,14 +1,30 @@
 import random
-computers_number = random.randint(1,100)
-prompt = "I have thought of a number. Take a guess. "
 
-while True:
-    players_guess = input(prompt)
-    if computers_number == int(players_guess):
-        print("Well done!")
-        break
-    elif computers_number > int(players_guess):
-        print("Too low. ")
-    else:
-       print("Too high. ")
-    prompt = "Guess again. "
+def f1():
+    computers_number = random.randint(1,100)
+    prompt = "I have thought of a number. Take a guess. "
+
+    while True:
+        players_guess = input(prompt)
+        if computers_number == int(players_guess):
+            print("Well done! ")
+            break
+        elif computers_number > int(players_guess):
+            print("Too low. ")
+        else:
+           print("Too high. ")
+        prompt = "Guess again. "
+
+def f2():
+    while True:
+        f1()
+        prompt = "Do you want to try again? n/Y "
+        players_input = input(prompt)
+        if players_input == "Y":
+            pass
+        else:
+            print("Thank you for playing!" )
+            break
+
+# ------- main
+f2()
